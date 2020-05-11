6594766dcac5c14d31f4a7fca3a89f14d13aa9b2
a8568730 <a8568730@hotmail.com>
2020-05-11 17:12:07 +0800

    * Hān-tiānn Kong-guân(西曆).

--- SquirrelApplicationDelegate.m
+++ SquirrelApplicationDelegate.m
@@ -141,7 +141,7 @@ void notification_handler(void* context_object, RimeSessionId session_id,
   // setup date format
   NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
 
-  // AM -> 上午
+  [formatter setCalendar: [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar]];
   [formatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"zh_Hans_TW"]];
   [formatter setTimeZone:[NSTimeZone timeZoneWithName:@"Asia/Taipei"]];
   [formatter setDateFormat:@"yyyy-MM-dd HH:mm"];
